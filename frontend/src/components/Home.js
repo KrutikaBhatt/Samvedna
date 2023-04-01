adf73c2db9e0ef58fd9e23f2a4c80f811923071a
kamalrohra <kamal.rohra@think360.ai>
2023-04-01 17:27:04 +0530

    Update: Home page almost done

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,64 @@
 import React, { useState } from "react";
-
+import { Modal } from "./Modal";
+import { Post } from "./Post";
 export const Home = () => {
   const [showOptions, setShowOptions] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [index, setIndex] = useState(0);
+  const openModalFunc = (index) => {
+    setIndex(index);
+    setOpenModal(!openModal);
+    console.log("This is the index that was clicked");
+    console.log(index);
+  };
+  const posts = [
+    {
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      author: "Michael Gough",
+      created_at: "15/02/2022",
+      comments: [
+        {
+          text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+          author: "Jese Leos",
+          created_at: "15/02/2022",
+        },
+        {
+          text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+          author: "Jese Leos",
+          created_at: "15/02/2022",
+        },
+        {
+          text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+          author: "Jese Leos",
+          created_at: "15/02/2022",
+        },
+      ],
+    },
+    {
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      author: "Michael Gough",
+      created_at: "15/02/2022",
+      comments: [
+        {
+          text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+          author: "Jese Leos",
+          created_at: "15/02/2022",
+        },
+      ],
+    },
+    {
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+      author: "Michael Gough",
+      created_at: "15/02/2022",
+      comments: [
+        {
+          text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+          author: "Jese Leos",
+          created_at: "15/02/2022",
+        },
+      ],
+    },
+  ];
   return (
     <div class="sm:ml-64">
       <section class="bg-white dark:bg-gray-900 py-8 lg:py-16">
@@ -11,7 +68,9 @@ export const Home = () => {
               Add Post
             </h2>
           </div>
-          <form class="mb-6">
+
+          <form class="mb-6 w-full relative">
+            <Modal openModal={openModal} setOpenModal={setOpenModal} />
             <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
               <label for="comment" class="sr-only">
                 Your comment
@@ -26,119 +85,27 @@ export const Home = () => {
             <button
               type="submit"
               class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-              Post comment
+              Post
             </button>
           </form>
           <div className="relative pt-4">
-            <article class="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
-              <footer class="flex justify-between items-center mb-2">
-                <div class="flex items-center">
-                  <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                    <img
-                      class="mr-2 w-6 h-6 rounded-full"
-                      src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
-                      alt="Michael Gough"
-                    />
-                    Michael Gough
-                  </p>
-                  <p class="text-sm text-gray-600 dark:text-gray-400">
-                    <time
-                      pubdate
-                      datetime="2022-02-08"
-                      title="February 8th, 2022">
-                      Feb. 8, 2022
-                    </time>
-                  </p>
-                </div>
-                <button
-                  onClick={() => setShowOptions(!showOptions)}
-                  id="dropdownComment1Button"
-                  data-dropdown-toggle="dropdownComment1"
-                  class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                  type="button">
-                  <svg
-                    class="w-5 h-5"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
-                  </svg>
-                  <span class="sr-only">Comment settings</span>
-                </button>
-
-                <div
-                  id="dropdownComment1"
-                  class={` ${
-                    !showOptions && "hidden"
-                  } absolute right-[-120px] top-0 w-36 bg-white rounded  divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600`}>
-                  <ul
-                    class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="dropdownMenuIconHorizontalButton">
-                    <li>
-                      <a
-                        href="#"
-                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                        Related to me
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </footer>
-
-              <p class="text-gray-500 dark:text-gray-400">
-                Very straight-to-point article. Really worth time reading. Thank
-                you! But tools are just the instruments for the UX designers.
-                The knowledge of the design tools are as important as the
-                creation of the design strategy.
-              </p>
-              <div class="flex items-center mt-4 space-x-4">
-                <button
-                  type="button"
-                  class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400">
-                  <svg
-                    aria-hidden="true"
-                    class="mr-1 w-4 h-4"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path>
-                  </svg>
-                  Reply
-                </button>
-              </div>
-            </article>
+            {posts.map((item, index) => {
+              return (
+                <Post
+                  index={index}
+                  onClick={openModalFunc}
+                  text={item.text}
+                  author={item.author}
+                  comments={item.comments}
+                  showOptions={showOptions}
+                  setShowOptions={setShowOptions}
+                  openModal={openModal}
+                  setOpenModal={setOpenModal}
+                />
+              );
+            })}
           </div>
-          <article class="p-6 mb-6 ml-6 lg:ml-12 text-base bg-white rounded-lg dark:bg-gray-900">
-            <footer class="flex justify-between items-center mb-2">
-              <div class="flex items-center">
-                <p class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                  <img
-                    class="mr-2 w-6 h-6 rounded-full"
-                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
-                    alt="Jese Leos"
-                  />
-                  Jese Leos
-                </p>
-                <p class="text-sm text-gray-600 dark:text-gray-400">
-                  <time
-                    pubdate
-                    datetime="2022-02-12"
-                    title="February 12th, 2022">
-                    Feb. 12, 2022
-                  </time>
-                </p>
-              </div>
-            </footer>
-            <p class="text-gray-500 dark:text-gray-400">
-              Much appreciated! Glad you liked it ☺️
-            </p>
-          </article>
+
           <article class="p-6 mb-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
             <footer class="flex justify-between items-center mb-2">
               <div class="flex items-center">
